223e03f429b161693b8f7f27ac9f63b3d5f8a192
Daniel Smith <danny@stormpoopersmith.com>
2014-02-26 20:40:23 +0000

    Changed if statements to method calls.

--- RomanKata/NumberConverter.cs
+++ RomanKata/NumberConverter.cs
@@ -1,32 +1,38 @@
-﻿namespace RomanKata
+﻿using System.Text;
+
+namespace RomanKata
 {
     public class NumberConverter
     {
         public string ToRomanNumeral(int number)
         {
-            var left = number;
-            var result = string.Empty;
+            var remaining = number;
+            var result = new StringBuilder();
 
-            if (left > 8)
-            {
-                result = "IX";
-                left -= 9;
-            }if (left > 4)
-            {
-                result = "V";
-                left -= 5;
-            }
-            if (left > 3)
-            {
-                result = "IV";
-                left -= 4;
-            }
-            for (var i = 0; i < left; i++)
-            {
-                result += "I";
-            }
+            remaining = AppendHighestRomanNumeral(remaining, 9, "IX", result);
+            remaining = AppendHighestRomanNumeral(remaining, 5, "V", result);
+            remaining = AppendHighestRomanNumeral(remaining, 4, "IV", result);
+            AddRemainingNumerals(remaining, result);
 
-            return result;
+            return result.ToString();
+        }
+
+        private static void AddRemainingNumerals(int remaining, StringBuilder result)
+        {
+            for (var i = 0; i < remaining; i++)
+            {
+                result.Append("I");
+            }
+        }
+
+        private static int AppendHighestRomanNumeral(int remaining, int value, string numeral, StringBuilder result)
+        {
+            if (remaining >= value)
+            {
+                result.Append(numeral);
+                remaining -= value;
+            }
+            return remaining;
         }
     }
 }
