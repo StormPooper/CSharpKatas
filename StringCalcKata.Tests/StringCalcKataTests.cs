7cbd98f0eae20e922e82441b8c6f4e2b4c6f7ad1
Daniel Smith <danny@stormpoopersmith.com>
2014-02-26 23:59:46 +0000

    Moved duplicate setup in tests

---
+++ StringCalcKata.Tests/StringCalcKataTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace StringCalcKata.Tests
+{
+    [TestFixture]
+    public class StringCalcKataTests
+    {
+        public StringCalculator Calculator;
+
+        [TestFixtureSetUp]
+        public void CreateCalculator()
+        {
+            Calculator = new StringCalculator();
+        }
+
+        [Test]
+        public void EmptyString_Total0()
+        {
+            var result = Calculator.Add(string.Empty);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void One_One_TotalTwo()
+        {
+            var result = Calculator.Add("1,1");
+            Assert.AreEqual(2, result);
+        }
+    }
+}
