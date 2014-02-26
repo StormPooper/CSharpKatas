efc3618aeb22fb6c5fd04780e6372e306bb63a96
Daniel Smith <danny@stormpoopersmith.com>
2014-02-26 23:23:40 +0000

    Made CalculateScore easier to read

--- BowlingKata.Tests/BowlingKataTests.cs
+++ BowlingKata.Tests/BowlingKataTests.cs
@@ -16,21 +16,21 @@ namespace BowlingKata.Tests
         [Test]
         public void AllRollsScore0_TotalScore0()
         {
-            RollTwiceForNumberOfFrames(0, 0, 10);
+            RollTwiceForNumberOfFrames(0, 0, numberOfFrames: 10);
             Assert.AreEqual(0, _scoreboard.CalculateScore());
         }
 
         [Test]
         public void AllRollsScore1_TotalScore20()
         {
-            RollTwiceForNumberOfFrames(1, 1, 10);
+            RollTwiceForNumberOfFrames(1, 1, numberOfFrames: 10);
             Assert.AreEqual(20, _scoreboard.CalculateScore());
         }
 
         [Test]
         public void AllFramesScore9_TotalScore90()
         {
-            RollTwiceForNumberOfFrames(5, 4, 10);
+            RollTwiceForNumberOfFrames(5, 4, numberOfFrames: 10);
             Assert.AreEqual(90, _scoreboard.CalculateScore());
         }
 
@@ -38,7 +38,7 @@ namespace BowlingKata.Tests
         public void StrikeInFirstFrameAndScore2InEachFrameAfter_TotalScore30()
         {
             RollStrikeForNumberOfFrames(1);
-            RollTwiceForNumberOfFrames(1, 1, 9);
+            RollTwiceForNumberOfFrames(1, 1, numberOfFrames: 9);
             Assert.AreEqual(30, _scoreboard.CalculateScore());
         }
 
@@ -46,22 +46,22 @@ namespace BowlingKata.Tests
         public void StrikeInEveryFrame_TotalScore300()
         {
             RollStrikeForNumberOfFrames(10);
-            RollTwiceForNumberOfFrames(10, 10, 1);
+            RollTwiceForNumberOfFrames(10, 10, numberOfFrames: 1);
             Assert.AreEqual(300, _scoreboard.CalculateScore());
         }
 
         [Test]
         public void SpareInFirstFrameAndScore2InEachFrameAfter_TotalScore29()
         {
-            RollTwiceForNumberOfFrames(5, 5, 1);
-            RollTwiceForNumberOfFrames(1, 1, 9);
+            RollTwiceForNumberOfFrames(5, 5, numberOfFrames: 1);
+            RollTwiceForNumberOfFrames(1, 1, numberOfFrames: 9);
             Assert.AreEqual(29, _scoreboard.CalculateScore());
         }
 
         [Test]
         public void SpareInAllFrames_TotalScore150()
         {
-            RollTwiceForNumberOfFrames(5, 5, 11);
+            RollTwiceForNumberOfFrames(5, 5, numberOfFrames: 11);
             Assert.AreEqual(150, _scoreboard.CalculateScore());
         }
 
--- BowlingKata/Scoreboard.cs
+++ BowlingKata/Scoreboard.cs
@@ -5,35 +5,56 @@ namespace BowlingKata
     public class Scoreboard
     {
         private const int NumberOfFrames = 10;
-        private const int MaxPerFrame = 10;
+        private const int MaximumScoreForFrame = 10;
 
         private readonly IList<int> _pins = new List<int>();
+        private int _currentRoll;
 
         public int CalculateScore()
         {
             var score = 0;
-            var currentRoll = 0;
-
+            _currentRoll = 0;
             for (var i = 0; i < NumberOfFrames; i++)
             {
-                if (_pins[currentRoll] == MaxPerFrame)
-                {
-                    score += _pins[currentRoll] + _pins[currentRoll + 1] + _pins[currentRoll + 2];
-                    currentRoll++;
-                }
-                else if (_pins[currentRoll] + _pins[currentRoll + 1] == MaxPerFrame)
-                {
-                    score += _pins[currentRoll] + _pins[currentRoll + 1] + _pins[currentRoll + 2];
-                    currentRoll += 2;
-                }
+                if (RolledAStrike(_currentRoll))
+                    score = ScoreStrike(score);
+                else if (RolledASpare(_currentRoll))
+                    score = ScoreSpare(score);
                 else
-                {
-                    score += _pins[currentRoll] + _pins[currentRoll + 1];
-                    currentRoll += 2;
-                }
+                    score = ScoreNormalFrame(score);
             }
-
             return score;  
+        }
+
+        private int ScoreStrike(int score)
+        {
+            score += _pins[_currentRoll] + _pins[_currentRoll + 1] + _pins[_currentRoll + 2];
+            _currentRoll++;
+            return score;
+        }
+
+        private int ScoreSpare(int score)
+        {
+            score += _pins[_currentRoll] + _pins[_currentRoll + 1] + _pins[_currentRoll + 2];
+            _currentRoll += 2;
+            return score;
+        }
+
+        private int ScoreNormalFrame(int score)
+        {
+            score += _pins[_currentRoll] + _pins[_currentRoll + 1];
+            _currentRoll += 2;
+            return score;
+        }
+
+        private bool RolledASpare(int currentRoll)
+        {
+            return _pins[currentRoll] + _pins[currentRoll + 1] == MaximumScoreForFrame;
+        }
+
+        private bool RolledAStrike(int currentRoll)
+        {
+            return _pins[currentRoll] == MaximumScoreForFrame;
         }
 
         public void Roll(int pins)
