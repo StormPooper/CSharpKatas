1bcc0638eb3965c066619c741b9fbbee065a5287
Daniel Smith <danny@stormpoopersmith.com>
2014-02-26 19:59:20 +0000

    Removed duplication.

--- RomanKata.Tests/RomanKataTests.cs
+++ RomanKata.Tests/RomanKataTests.cs
@@ -5,25 +5,27 @@ namespace RomanKata.Tests
     [TestFixture]
     public class RomanKataTests
     {
+        private NumberConverter _converter;
+
+        [TestFixtureSetUp]
+        public void NumberConverterSetup()
+        {
+            _converter = new NumberConverter();
+        }
+
         [Test]
         public void One_Becomes_I()
         {
-            const int numberToConvert = 1;
-            var converter = new NumberConverter();
-
-            var result = converter.ToRomanNumeral(numberToConvert);
-
-            Assert.AreEqual("I", result);
+            const int number = 1;
+            var romanNumerals = _converter.ToRomanNumeral(number);
+            Assert.AreEqual("I", romanNumerals);
         }
 
         [Test]
         public void Two_Becomes_II()
         {
-            const int numberToConvert = 2;
-            var converter = new NumberConverter();
-
-            var result = converter.ToRomanNumeral(numberToConvert);
-
+            const int number = 2;
+            var result = _converter.ToRomanNumeral(number);
             Assert.AreEqual("II", result);
         }
     }
