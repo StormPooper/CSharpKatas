f1578da174f5451f257670b1da459add29e57b72
Daniel Smith <danny@stormpoopersmith.com>
2014-02-27 00:48:23 +0000

    Made delimiter code more readable

--- StringCalcKata/StringCalculator.cs
+++ StringCalcKata/StringCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -6,29 +7,37 @@ namespace StringCalcKata
 {
     public class StringCalculator
     {
-        private static readonly string[] DefaultDelimiters = { ",", "\n" };
+        private static readonly string[] DefaultDelims = { ",", "\n" };
+        private const string DelimStart = "//";
+        private const string DelimEnd = "\n";
+        private static readonly string[] DelimEnds = { DelimEnd };
 
         public int Add(string sum)
         {
             if (sum == string.Empty) return 0;
             var calculation = new StringBuilder(sum);
-            var delimiter = Delimiter(calculation);
             var total = 0;
-            foreach (var value in calculation.ToString().Split(delimiter, StringSplitOptions.None).Select(number => Convert.ToInt32(number)))
+            foreach (var number in NumbersFromString(calculation))
             {
-                if(value < 0) throw new ArgumentOutOfRangeException(sum, "Negatives are not allowed.");
-                if(value > 1000) continue;
-                total += value;
+                if(number < 0) throw new ArgumentOutOfRangeException(sum, "Negatives are not allowed.");
+                if(number > 1000) continue;
+                total += number;
             }
             return total;
+        }
+
+        private static IEnumerable<int> NumbersFromString(StringBuilder calculation)
+        {
+            var delimiter = Delimiter(calculation);
+            return calculation.ToString().Split(delimiter, StringSplitOptions.None).Select(number => Convert.ToInt32(number));
         }
 
         private static string[] Delimiter(StringBuilder sum)
         {
             var calculation = sum.ToString();
-            if (!calculation.StartsWith("//")) return DefaultDelimiters;
-            var delimiter = new[] { calculation.Split(new[] {"\n"}, StringSplitOptions.None).First().Remove(0, 2) };
-            sum.Remove(0, 3 + delimiter[0].Length);
+            if (!calculation.StartsWith(DelimStart)) return DefaultDelims;
+            var delimiter = new[] { calculation.Split(DelimEnds, StringSplitOptions.None).First().Remove(0, DelimStart.Length) };
+            sum.Remove(0, DelimStart.Length + DelimEnd.Length + delimiter[0].Length);
             return delimiter;
         }
     }
