9c753360465db15e4e31f5340e5a3a9504b64f8e
Daniel Smith <danny@stormpoopersmith.com>
2014-02-27 00:04:49 +0000

    Moved

--- StringCalcKata.Tests/StringCalcKataTests.cs
+++ StringCalcKata.Tests/StringCalcKataTests.cs
@@ -13,25 +13,13 @@ namespace StringCalcKata.Tests
             Calculator = new StringCalculator();
         }
 
-        [Test]
-        public void EmptyString_Total0()
+        [TestCase("", 0)]
+        [TestCase("1,1", 2)]
+        [TestCase("2,2", 4)]
+        public void CalculateAdditionFromString(string sum, int expected)
         {
-            var result = Calculator.Add(string.Empty);
-            Assert.AreEqual(0, result);
-        }
-
-        [Test]
-        public void One_One_TotalTwo()
-        {
-            var result = Calculator.Add("1,1");
-            Assert.AreEqual(2, result);
-        }
-
-        [Test]
-        public void Two_Two_TotalFour()
-        {
-            var result = Calculator.Add("2,2");
-            Assert.AreEqual(4, result);
+            var result = Calculator.Add(sum);
+            Assert.AreEqual(expected, result);
         }
     }
 }
