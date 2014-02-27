3cf477392a9b0325070e29a1a1b69f00a47c1416
Daniel Smith <danny@stormpoopersmith.com>
2014-02-27 00:19:13 +0000

    Extract delimiter to method

--- StringCalcKata/StringCalculator.cs
+++ StringCalcKata/StringCalculator.cs
@@ -8,13 +8,19 @@ namespace StringCalcKata
         public int Add(string sum)
         {
             if (sum == string.Empty) return 0;
-            var separator = new []{ ',', '\n' };
+            var delimiter = SetDelimiter(ref sum);
+            return sum.Split(delimiter).Sum(number => Convert.ToInt32(number));
+        }
+
+        private static char[] SetDelimiter(ref string sum)
+        {
+            var separator = new[] {',', '\n'};
             if (sum.StartsWith("//"))
             {
                 separator = sum.ToCharArray(2, 1);
                 sum = sum.Remove(0, 4);
             }
-            return sum.Split(separator).Sum(number => Convert.ToInt32(number));
+            return separator;
         }
     }
 }
