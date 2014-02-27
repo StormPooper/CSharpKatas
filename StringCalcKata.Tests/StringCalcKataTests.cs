b2faaeeeb1031bf4d7c912ce3d782766122871ac
Daniel Smith <danny@stormpoopersmith.com>
2014-02-27 00:10:27 +0000

    Added testcase, included bigger numbers

--- StringCalcKata.Tests/StringCalcKataTests.cs
+++ StringCalcKata.Tests/StringCalcKataTests.cs
@@ -24,11 +24,13 @@ namespace StringCalcKata.Tests
             Assert.AreEqual(expected, result);
         }
 
-        [Test]
-        public void CalculateAnyNumberSum()
+        [TestCase("2,2,2", 6)]
+        [TestCase("8,16,32", 56)]
+        [TestCase("8,16,32,64,128", 248)]
+        public void CalculateAnyNumberSum(string sum, int expected)
         {
-            var result = Calculator.Add("2,2,2");
-            Assert.AreEqual(6, result);
+            var result = Calculator.Add(sum);
+            Assert.AreEqual(expected, result);
         }
     }
 }
