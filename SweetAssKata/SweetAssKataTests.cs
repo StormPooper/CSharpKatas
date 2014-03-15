21c8a4205e22eb61966ff60c9ba0b67acb1e37c6
Daniel Smith <danny@stormpoopersmith.com>
2014-03-15 21:42:47 +0000

    Combined tests.

--- SweetAssKata/SweetAssKataTests.cs
+++ SweetAssKata/SweetAssKataTests.cs
@@ -5,25 +5,13 @@ namespace SweetAssKata
     [TestFixture]
     public class SweetAssKataTests
     {
-        [Test]
-        public void SweetAss_Car_Becomes_Sweet_AssCar()
+        [TestCase("sweet-ass car", "sweet ass-car")]
+        [TestCase("nice-ass car", "nice ass-car")]
+        [TestCase("Man, that's a sweet-ass car.", "Man, that's a sweet ass-car.")]
+        public void SentenceContainsSomethingAssSomething_HyphenIsMoved(string sentence, string expected)
         {
-            var result = Hyphenator.MoveHyphen("sweet-ass car");
-            Assert.AreEqual("sweet ass-car", result);
-        }
-
-        [Test]
-        public void NiceAss_Car_Becomes_Nice_AssCar()
-        {
-            var result = Hyphenator.MoveHyphen("nice-ass car");
-            Assert.AreEqual("nice ass-car", result);
-        }
-
-        [Test]
-        public void ManThatsASweetAss_Car_Becomes_ManThatsASweet_AssCar()
-        {
-            var result = Hyphenator.MoveHyphen("Man, that's a sweet-ass car.");
-            Assert.AreEqual("Man, that's a sweet ass-car.", result);
+            var result = Hyphenator.MoveHyphen(sentence);
+            Assert.AreEqual(expected, result);
         }
     }
 }
